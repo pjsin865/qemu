cdbdc4a1b48e0f9dfbb855b95bd08bf09d3e437e
Jonsghin Park <pjsin865@coasia.com>
2026-04-21 16:51:36 +0900

    change path for image(uboot/kernel)
    Signed-off-by: Jonsghin Park <pjsin865@coasia.com>

--- run_qemu_uboot_kernel.sh
+++ run_qemu_uboot_kernel.sh
@@ -3,8 +3,8 @@ qemu-system-aarch64 \
     -cpu cortex-a53 \
     -m 1024M \
     -nographic \
-    -bios output/images/u-boot.bin \
-    -kernel output/images/Image \
+    -bios ./buildroot/output/images/u-boot.bin \
+    -kernel ./buildroot/output/images/Image \
     -append "root=/dev/vda console=ttyAMA0" \
-    -drive file=output/images/rootfs.ext4,format=raw,if=virtio
+    -drive file=./buildroot/output/images/rootfs.ext4,format=raw,if=virtio
 
